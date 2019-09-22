09b9c258bd848ae3d2e069972ce17b84734982d1
Pby-25 <agains1595@gmail.com>
2019-09-21 22:47:56 -0700

    small progress

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -6,24 +6,40 @@ class RequestsController < ApplicationController
   def create
     # create based on user_id (automatic), place_id... for now
     # if station DNE, create one 
-    requested_station = Station.find_or_create_by(place_id: params[:placeId])
-    requested_station.expiry_date = Time.zone.advance(days:15)
-    # update link in station controller
-    puts("this")
-
-    puts (params[:thing])
-
-    puts("that")
-    response = params[:gogogo]
+    requested_station = Station.find_or_create_by(place_id: request_params[placeId])
+    requested_station.expiry_date = Time.zone.now.advance(days: 15) # duration
+    if requested_station.save
+      request = current_user.requests.build(request_params)
+      request.save
+      response = {saved: true}
+    else
+      response = {saved: false}
+    end
     render json: response
   end
 
   def update
     # update expiry date
+    requested_station = Station.find_by(place_id: request_params[placeId])
+    requested_station.expiry_date = Time.zone.now.advance(days: 15) 
+    if requested_station.save
+      request = current_user.requests.find_by(place_id: request_params[placeId])
+      request.touch
+      response = {saved: true}
+    else
+      response = {saved: false}
+    end
+    render json: response
+    # flawed, keep like this for now
   end
 
   def destroy
   end
 
+  private
+
+    def request_params
+      params.require(:request).permit(:place_id, :duration, :nickname)
+    end
 end
 
